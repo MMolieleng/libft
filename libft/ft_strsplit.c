75db38abdcaaac8893f8afaf5e6f801526e17f73
MMolieleng <mmoliele@e4r4p2.wtc.co.za>
2017-08-16 14:13:54 +0200

    added missing functions

---
+++ libft/ft_strsplit.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emsimang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/19 14:36:24 by emsimang          #+#    #+#             */
+/*   Updated: 2017/08/07 11:16:28 by mmoliele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+int		get_word_count(char *s, int c)
+{
+	int i;
+	int	words;
+
+	i = 0;
+	words = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c && s[i])
+			i++;
+		if (s[i] != '\0')
+			words++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (words);
+}
+
+int		get_word_len(char *s, int c, int index)
+{
+	int	end;
+	int	start;
+	int	word_len;
+
+	end = 0;
+	word_len = 0;
+	start = index;
+	while (s[index] != c && s[index])
+	{
+		index++;
+		end = index;
+	}
+	word_len = end - start;
+	return (word_len);
+}
+
+int     hasword(char *s, int c)
+{
+	if (get_word_count(s, c))
+		return (1);
+	return (0);
+}
+
+char	**ft_strsplit(char *str, int c)
+{
+	int i;
+	int	j;
+	int	index;
+	char *word;
+	char **sptr;
+
+	i = 0;
+	j = 0;
+	sptr = 0;
+	word = 0;
+	sptr = (char **)malloc(sizeof(char *) * (get_word_count(str, c) + 1));
+	if (hasword(str, c))
+	{
+		while (str[i] != '\0')
+		{
+			index = 0;
+			while (str[i] == c && str[i] != '\0')
+				i++;
+			if (str[i] != '\0')
+			{
+				word = (char *)malloc(sizeof(char) * (get_word_len(str, c, i) + 1));
+				while (str[i] != c && str[i] != '\0')
+				{
+					word[index] = str[i];
+					i++;
+					index++;
+				}
+				word[index] = '\0';
+				sptr[j] = word;
+				j++;
+			}
+		}
+	}
+	sptr[j] = 0;
+	return (sptr);
+}
---
+++ libft/ft_strsub.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoliele <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/04 10:02:37 by mmoliele          #+#    #+#             */
+/*   Updated: 2016/12/01 11:30:35 by mmoliele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char		*sub;
+	size_t		i;
+
+	i = 0;
+	if ((sub = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	while (s[start] != '\0' && i < len)
+	{
+		sub[i] = s[start];
+		i++;
+		start++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
